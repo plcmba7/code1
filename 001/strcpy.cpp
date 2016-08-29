70bbaedd94ebf13a9b976836393dcb635847cbbd
plcmba7 <plcmba@163.com>
2016-08-29 14:55:35 +0800

    Update strcpy.cpp

--- 001/strcpy.cpp
+++ 001/strcpy.cpp
@@ -1,29 +1,29 @@
-//#include<iostream>
-//using namespace std;
-//#include<assert.h>
-//
-//char * strcpyzr(char *des,const char*src)
-//{
-//assert(des!=NULL&&src!=NULL);
-//
-//	char *p;
-//	p=des;
-//	while(*src!=0)
-//	{
-//		*des=*src;
-//		src++;
-//		des++;
-//	}
-//	*des=0;
-//	return p;
-//}
-//
-//
-//int main()
-//{
-//	char a[20]="heheleell";
-//	char b[10]="hello";
-//	strcpyzr(a,b);
-//	cout<<a<<endl;
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+#include<assert.h>
+
+char * strcpyzr(char *des,const char*src)
+{
+assert(des!=NULL&&src!=NULL);
+
+	char *p;
+	p=des;
+	while(*src!=0)
+	{
+		*des=*src;
+		src++;
+		des++;
+	}
+	*des=0;
+	return p;
+}
+
+
+int main()
+{
+	char a[20]="heheleell";
+	char b[10]="hello";
+	strcpyzr(a,b);
+	cout<<a<<endl;
+	return 0;
+}
